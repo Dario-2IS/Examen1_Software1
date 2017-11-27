c3bc4d427b5d68f4724a8bf2b130419ce304e784
Dario-2IS <adjimenez1@espe.edu.ec>
2017-11-27 17:00:16 -0500

    Examen1

---
+++ Procesos/Procesos/Calculo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procesos
+{
+    public class Calculo
+    {
+
+        public double promedio(string nota1, string nota2, string nota3) {
+            double media;
+            double nt1, nt2, nt3;
+            nt1 = Validacion(nota1);
+            nt2 = Validacion(nota2);
+            nt3 = Validacion(nota3);
+            if (nt1 >= 0 && nt1 <= 20 && nt2 >= 0 && nt2 <= 20 && nt3 >= 0 && nt3 <= 20)
+            {
+                media = (nt1 + nt2 + nt3) / 3;
+                return media;
+            }
+            return -1;
+        }
+
+        public string estadoMateria(string promedioMateria) {
+            double promedio;
+            promedio = Validacion(promedioMateria);
+            if (promedio > 14)
+                return "A";
+            else if (promedio > 12 && promedio < 14)
+                return "S";
+            else
+                return "P";
+        }
+
+        public string estadoSemestre(string estadoMateria1, string estadoMateria2) {
+            if (estadoMateria1 == "A" || estadoMateria1 == "a" && estadoMateria2 == "A" || estadoMateria2 == "a")
+                return "Aprobado";
+            else if (estadoMateria2 == "P" || estadoMateria1 == "p" && estadoMateria2 == "P" || estadoMateria1 == "p")
+                return "Reprobado";
+            else
+                return "ERROR";
+        }
+
+
+        public double Validacion(string sueldoA)
+        {
+            double num;
+            try
+            {
+                num = Double.Parse(sueldoA);
+                return num;
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+
+    }
+}
---
+++ Procesos/UnitTestCalculo/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Procesos;
+
+namespace UnitTestCalculo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        //PROMEDIO
+        [TestMethod]
+        public void TestMethodNumeros()
+        {
+            Calculo calculo = new Calculo();
+            double expectedR = 15;
+            double actualR;
+            actualR = calculo.promedio("15","15","15");
+            Assert.AreEqual(expectedR, actualR);
+        }
+
+
+        [TestMethod]
+        public void TestMethodLetras()
+        {
+            Calculo calculo = new Calculo();
+            double expectedR = -1;
+            double actualR;
+            actualR = calculo.promedio("0", "a", "0");
+            Assert.AreEqual(expectedR, actualR);
+        }
+
+
+        [TestMethod]
+        public void TestMethodNumerosNegativos()
+        {
+            Calculo calculo = new Calculo();
+            double expectedR = -1;
+            double actualR;
+            actualR = calculo.promedio("-15.8", "-12", "a");
+            Assert.AreEqual(expectedR, actualR);
+        }
+
+        //ESTADO MATERIA
+
+        [TestMethod]
+        public void TestMethodEstadoError()
+        {
+            Calculo calculo = new Calculo();
+            string expectedR = "P";
+            string actualR;
+            actualR = calculo.estadoMateria("-15.8");
+            Assert.AreEqual(expectedR, actualR);
+        }
+
+        [TestMethod]
+        public void TestMethodEstadoAprobado()
+        {
+            Calculo calculo = new Calculo();
+            string expectedR = "A";
+            string actualR;
+            actualR = calculo.estadoMateria("18,9");
+            Assert.AreEqual(expectedR, actualR);
+        }
+
+        [TestMethod]
+        public void TestMethodEstadoReprobado()
+        {
+            Calculo calculo = new Calculo();
+            string expectedR = "P";
+            string actualR;
+            actualR = calculo.estadoMateria("8,9");
+            Assert.AreEqual(expectedR, actualR);
+        }
+
+        //ESTADO SEMESTER
+
+        [TestMethod]
+        public void TestMethodEstadoSemestreAprobado()
+        {
+            Calculo calculo = new Calculo();
+            string expectedR = "Aprobado";
+            string actualR;
+            actualR = calculo.estadoSemestre("A","A");
+            Assert.AreEqual(expectedR, actualR);
+        }
+
+    }
+}
